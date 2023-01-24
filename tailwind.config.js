e79eae9aa9a5408624ff3025b477708670248e66
floretcl <floretcl@outlook.com>
2023-01-24 19:21:30 +0100

    add custom colors and font-lato -> font-sans (default)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'red': '#fe3a44',
+      'light-red': 'ff4750',
+      'light-gray': '#f7f7f7',
+      'dark-gray': '#cdcdcd',
+      'white': '#ffffff',
+      'black': '#000000'
+
+    },
     extend: {
       fontFamily: {
-        'lato': ['"Lato"', 'sans-serif']
+        'sans': ['"Lato"', defaultTheme.fontFamily.sans],
       },
     },
   },
