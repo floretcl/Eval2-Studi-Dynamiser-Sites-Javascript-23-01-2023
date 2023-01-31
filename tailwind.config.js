8bc2932d156dcb42f32707ae68700896dbb3493e
floretcl <floretcl@outlook.com>
2023-01-31 17:03:30 +0100

    add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,23 @@ module.exports = {
       'red': '#fe3a44',
       'light-red': 'ff4750',
       'light-gray': '#efefef',
-      'dark-gray': '#b3b3b3',
+      'dark-gray': '#737373',
       'white': '#ffffff',
       'black': '#000000'
     },
     extend: {
       fontFamily: {
         'sans': ['"Lato"', defaultTheme.fontFamily.sans],
+      },
+      animation: {
+        'ping-low': 'pinglow 1.5s cubic-bezier(0, 0, 0.2, 1) infinite',
+      },
+      keyframes: {
+        pinglow: {
+          '0%': { transform: 'scale(1.15)', opacity: '1' },
+          '50%': { transform: 'scale(1)', opacity: '0.5' },
+          '100%': { transform: 'scale(1.15)', opacity: '1' },
+        },
       },
     },
   },
